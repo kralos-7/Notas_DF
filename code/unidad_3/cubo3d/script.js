e9cf3f84f72012d8e696b79fc6c1d1b1a1dbd9ed
kralos-7 <c_rojasx@hotmail.com>
2026-04-15 09:35:49 -0600

    Update code

--- code/unidad_3/cubo3d/script.js
+++ code/unidad_3/cubo3d/script.js
@@ -1,23 +1,46 @@
 const cube = document.getElementById("cube");
 
+let scale = 1; // 100%
+
 let rotateX = 0;
 let rotateY = 0;
 
 document.addEventListener("keydown", (e) => {
-  switch (e.key) {
-    case "ArrowUp":
-      rotateX -= 90;
-      break;
-    case "ArrowDown":
-      rotateX += 90;
-      break;
-    case "ArrowLeft":
-      rotateY -= 90;
-      break;
-    case "ArrowRight":
-      rotateY += 90;
-      break;
-  }
 
-  cube.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+	switch (e.key) {
+    	case "ArrowUp":
+      		rotateX -= 90;
+      		break;
+    	case "ArrowDown":
+      		rotateX += 90;
+      		break;
+    	case "ArrowLeft":
+      		rotateY -= 90;
+      		break;
+    	case "ArrowRight":
+      		rotateY += 90;
+      		break;
+ 	}
+
+  	// cube.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+  	// 🔍 ESCALA
+  	if (e.key.toLowerCase() === "a") {
+    	scale += 0.2;
+    	if (scale > 2) scale = 2; // máximo 200%
+  	}
+
+  	if (e.key.toLowerCase() === "d") {
+    	scale -= 0.2;
+    	if (scale < 0.5) scale = 0.5; // mínimo 50%
+  	}
+
+	//cube.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(${scale})`;
+
+	cube.style.transform = `
+  rotateX(${rotateX}deg)
+  rotateY(${rotateY}deg)
+  scale3d(${scale}, ${scale}, ${scale})
+`;
+
 });

86992c7d397d4d76b3a6a54c4f4b50dd0cd286b2
Kralos <c_rojasx@hotmail.com>
2026-05-25 18:33:47 -0600

    update code

--- code/unidad_5/examples_React/mi-catalogo-react/src/App.jsx
+++ code/unidad_5/examples_React/mi-catalogo-react/src/App.jsx
@@ -3,28 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-
 import products from './data/productos'
 import CategorySection from './components/CategorySection'
 
 function App() {
-
   const categories = [
     "Tecnología",
     "Hogar",
     "Deportes",
     "Ropa"
   ]
-
   return (
     <div className="container py-5">
-
       <h1 className="text-center mb-5">
         Catálogo de Productos
       </h1>
-
       {categories.map(category => (
-
         <CategorySection
           key={category}
           title={category}
